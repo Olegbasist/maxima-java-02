15f7d91befb5c29e44b9992be37894037e6d7ae2
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-21 16:54:22 +0700

    Homework 2 Сделал Plane наследником Transport. Работатет )))

--- src/main/java/org/example/Plane.java
+++ src/main/java/org/example/Plane.java
@@ -1,85 +1,20 @@
 package org.example;
 
-//12.09.2022  Описать класс Transport (транспортное средство) со свойствами:
-//        name — имя, строка.
-//        capacity — грузоподъемность, число.
-//        speed — скорость, число.
-//        costOfKm — стоимость за километр, дробное число
 
-// TODO: 15.09.2022 Для корабля и самолета при невозможности перевозки в указанный город
-//                  метод float getPrice(City city) должен возвращать 0.
-
-public class Plane
-{
-    public Plane() {
-    }
-    private String name; //Название
-    private int capacity; //Вместимость
-    private int speed; //Скорость
-    private float costOfKm;//Стоимость километра
-
-    // Конструктор - один за всех
-    public Plane(String name, int capacity, int speed, float costOfKm) {
-        this.name = name;
-        this.capacity = capacity;
-        this.speed = speed;
-        this.costOfKm = costOfKm;
-    }
-
-
-
-    // Геттеры-сеттеры
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public float getCostOfKm() {
-        return costOfKm;
-    }
-
-    public void setCostOfKm(float costOfKm) {
-        this.costOfKm = costOfKm;
-    }
-
-    // Вывод получившегося транспорта
-    public void printVehicleFeacher(){
-        System.out.println(name+ " вмещает " +capacity+ "Кг. груза, летит со скоростью " +speed+ "Км/ч и стоит " +costOfKm+ " рубля за 1Км. пути");
-    }
-
-
-
+public class Plane extends Transport{
 
     // Подсчёт стоимости перевозки
     public float getPrice(City city){
         if (city.isHasAirport()) {
-            return (costOfKm * city.getDistanceKm());
+            return (getCostOfKm() * city.getDistanceKm());
         }
         else {
             return 0;
         }
 
     }
+
+
 
 
 }
