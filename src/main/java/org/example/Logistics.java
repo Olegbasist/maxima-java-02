21d909282320c4ec878b05a16e1ca53a5d57cc54
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-10-04 11:00:41 +0700

    Update Logistics.java

--- src/main/java/org/example/Logistics.java
+++ src/main/java/org/example/Logistics.java
@@ -3,7 +3,7 @@ package org.example;
 // TODO: 22.09.2022 Описать класс Logistics - бизнес логику нашей логистики,
 //  при создании которого мы будем передавать ему некоторое количество объектов класса Transport ,
 //  заполняя свойство vehicles.
-//  Пример??? (getSum2(List<Account> accounts))
+//
 //
 // TODO: 22.09.2022 Описать методы Transport getShipping(City city, int weight, int hours) - найти самое оптимальное по стоимости доставки транспортное средство.
 //
