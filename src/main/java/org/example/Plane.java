cebf1e6c3b69f0011c06d75903ef1b1d299d2e51
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-10-28 20:10:55 +0700

    Исправил Plane
    Добавил пропавший конструктор

--- src/main/java/org/example/Plane.java
+++ src/main/java/org/example/Plane.java
@@ -3,6 +3,10 @@ package org.example;
 
 public class Plane extends Transport{
 
+    public Plane(String name, int capacity, int speed, float costOfKm) {
+        super(name, capacity, speed, costOfKm);
+    }
+
     // Подсчёт стоимости перевозки
     public float getPrice(City city){
         if (city.isHasAirport()) {
