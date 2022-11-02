0d27886bf3f58dd23c5b257f5f459a66cd24d91e
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-11-02 21:59:43 +0700

    Сделал методы для производства корабля и самолёта

--- src/main/java/org/example/TransportFactory.java
+++ src/main/java/org/example/TransportFactory.java
@@ -11,38 +11,51 @@ package org.example;
 
 public class TransportFactory {
 
-    private final String shipName = "Сухогруз 'Доктор наук профессор Шварценгольд'";
-    private final String airplaneName = "Грузовой самолёт 'Сибирский буревестник'";
-    private final int weightMultiplicity = 500;
-    private final int speedMultiplicity = 10;
-
     public Transport getTransport(City city, int weight, int hours){
         float requiredSpeed = city.getDistanceKm()/hours;
 
 
 
-        return makeTruck(getRightCapacity(weight), getRightSpeed(requiredSpeed));
+        return getTruck(getRightCapacity(weight), getRightSpeed(requiredSpeed));
     }
 
     private int getRightSpeed (float requiredSpeed){
-        int speed = requiredSpeed<=speedMultiplicity? speedMultiplicity
-                : requiredSpeed/speedMultiplicity == requiredSpeed%speedMultiplicity ? (int) requiredSpeed //Кажется бессмысленно лишний код... всё равно 2 операции - можно было просто посчитать последним условием
-                : (int) (Math.ceil((float) requiredSpeed/speedMultiplicity)*speedMultiplicity);
+        final int speedMultiplicity = 10;
+
+        int speed = requiredSpeed<= speedMultiplicity ? speedMultiplicity
+                : requiredSpeed/ speedMultiplicity == requiredSpeed% speedMultiplicity ? (int) requiredSpeed //Кажется бессмысленно лишний код... всё равно 2 операции - можно было просто посчитать последним условием
+                : (int) (Math.ceil((float) requiredSpeed/ speedMultiplicity)* speedMultiplicity);
         return speed;
     }
 
     private int getRightCapacity (int weight){
+        final int weightMultiplicity = 500;
+        
         int capacity = weight <= weightMultiplicity ? weightMultiplicity
-                : (float) weight/weightMultiplicity == weight%weightMultiplicity ? weight //Кажется бессмысленно лишний код... всё равно 2 операции - можно было просто посчитать последним условием
-                : (int) (Math.ceil((float) weight/weightMultiplicity))*weightMultiplicity;
+                : (float) weight/ weightMultiplicity == weight% weightMultiplicity ? weight //Кажется бессмысленно лишний код... всё равно 2 операции - можно было просто посчитать последним условием
+                : (int) (Math.ceil((float) weight/ weightMultiplicity))* weightMultiplicity;
         return capacity;
     }
 
-    private Transport makeTruck (int capacity, int speed ){
+    private Transport getTruck (int capacity, int speed ){
 
         final float costOfKm = 2.5f;
         final String trackName = "Автопоезд 'Нарядный купальщик'";
         return new Truck(trackName,capacity, speed, costOfKm);
+    }
+
+    private Transport getShip (int capacity, int speed ){
+
+        final float costOfKm = 1.7f;
+        String shipName = "Сухогруз 'Доктор наук профессор Шварценгольд'";
+        return new Ship(shipName,capacity, speed, costOfKm);
+    }
+
+    private Transport getPlane (int capacity, int speed ){
+
+        final float costOfKm = 7.8f;
+        String airplaneName = "Грузовой самолёт 'Сибирский буревестник'";
+        return new Plane(airplaneName,capacity, speed, costOfKm);
     }
 
 
