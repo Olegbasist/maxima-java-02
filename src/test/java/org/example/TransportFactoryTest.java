a33dde82cd4075bcede0d5830cc490db807f773e
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-01-07 22:04:53 +0700

    Тест проверяет правильность создания типа транспорта

--- src/test/java/org/example/TransportFactoryTest.java
+++ src/test/java/org/example/TransportFactoryTest.java
@@ -1,6 +1,6 @@
 package org.example;
 
-// TODO:- Название и стоимость — для самолета, корабля и грузовика различные константы
+//      - Название и стоимость — для самолета, корабля и грузовика различные константы
 //              , выберите их сами, задайте их в начале метода.
 //      - Если скорость, необходимая для выполнения заказа до 40 км/ч и город на воде, то корабль.
 //      Если нужна скорость выше 120 км/ч, и есть аэропорт, то самолет.
@@ -10,35 +10,52 @@ package org.example;
 //       - Скорость должна равняться скорости, необходимой для прибытия в срок, округленной вверх до ближайшего десятка.
 //       - Если вес кратен 500 и или скорость кратна 10, то округлять не нужно.
 
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.sound.midi.Track;
+
+import static org.junit.Assert.*;
 
 public class TransportFactoryTest {
-    private TransportFactory transportFactory = new TransportFactory();
+    private TransportFactory transportFactory; //= new TransportFactory();
     private City city; //= new City("City",1);
     private Transport transport; // = transportFactory.getTransport(city,1,1);
 
     @Before
     public void prepare () {
+        transportFactory = new TransportFactory();
         city = new City("City",1);
-        transport = getTransport(city,1,1);
+        transport = getTransportFromTransportFactory();
     }
-    private Transport getTransport (City city, int weight, int hours) {
-        return transportFactory.getTransport(city,weight,hours);
+    private Transport getTransportFromTransportFactory() {
+        return transportFactory.getTransport(city, 1, 1);
     }
 
-    /*@Test
-    public void shouldGetTrack () {
-        //Transport transport = transportFactory.getTransport(city,1,1);
-        System.out.println(transport);
-        //System.out.println(getTransport(city,1,1).getName());
-    }*/
     @Test
     public void shouldBeNotNulCity () {
-        
+        assertNotNull(city);
+    }
+    @Test
+    public void shouldBeNotNullTransport () {
+        assertNotNull(transport);
+    }
+    @Test
+    public void shouldMakeTrack () {
+        city.setOnWater(false);
+        assertSame(Truck.class, getTransportFromTransportFactory().getClass());
+    }
+    @Test
+    public void shouldMakeShip () {
+        Ship ship = new Ship("Ship",1,1,1);
+        assertSame(Ship.class, getTransportFromTransportFactory().getClass());
+    }
+    @Test
+    public void shouldMakePlane () {
+        city.setHasAirport(true);
+        city.setDistanceKm(121);
+        assertSame(Plane.class, getTransportFromTransportFactory().getClass());
     }
 
 }
